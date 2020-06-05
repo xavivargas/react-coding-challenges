50694934ecbb6808c5765f89a84074fc640d3a4a
Marcello <mdgozza@gmail.com>
2020-06-05 12:12:04 -0400

    add toggle and reqs

--- rocket-ship/src/routes/LaunchPad/index.js
+++ rocket-ship/src/routes/LaunchPad/index.js
@@ -1,1 +1,22 @@
-export { default } from './components/LaunchPad';
+import * as React from 'react'
+import {ClassRocket, FunctionalRocket} from './components/Rocket/'
+import LaunchPad from './components/LaunchPad';
+
+export default function Picker(){
+    const [Rocket, setRocket] = React.useState(() => ClassRocket);
+
+    function toggleLaunchPad(){
+
+        setRocket(Rocket => Rocket === ClassRocket ? FunctionalRocket : ClassRocket)
+    }
+
+    return <div>
+        <LaunchPad Rocket={Rocket}/>
+        <div style={{display: "flex"}}>
+        <h4 style={{marginRight: "1rem"}}>{Rocket === ClassRocket ? "Class Rocket" : "Functional Rocket"}</h4>
+        <button onClick={toggleLaunchPad}>Toggle LaunchPad</button>
+        </div>
+        
+    </div>
+
+}
